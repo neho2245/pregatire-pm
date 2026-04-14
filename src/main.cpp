b2415119c039937127128c19dec83a953fa177ab
neho <alexneho@yahoo.com>
2026-04-14 11:27:32 +0300

    Solved task_2 from lab0
    This toggles the LED to blink or keep its state based on the press of button PB2

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,18 @@ int main() {
 	/* Setăm pinul 7 al portului D ca pin de ieșire. */
 	DDRD |= (1 << PD7);
  
+  int led_is_blinking = 0;
 	while(1) {
 
     // check that the button was pressed
     // which means that the value on PB2 is 0 (the button connects the pin to the ground)
     if (! (PINB & (1 << PB2))) {
+      // toggle the led_is_blinking
+      led_is_blinking = led_is_blinking ^ 1;
+      _delay_ms(500);
+    }
+    
+    if (led_is_blinking) {
       PORTD ^= (1 << PD7);
       _delay_ms(500);    
     }
